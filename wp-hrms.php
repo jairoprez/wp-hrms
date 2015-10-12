1ca893a873df8e320347704a802da571d94a85ed
Jairo Perez <jairo.preztaveras@gmail.com>
2015-10-12 00:09:25 -0400

    add: main plugin php file.

---
+++ wp-hrms.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: WP HRMS
+ * Plugin URI: http://perezlabs.com
+ * Description: Human Resource Management System for WordPress
+ * Version: 0.1.0
+ * Author: Perez Labs
+ * Author URI: http://perezlabs.com
+ * License: GNU General Public License version 3.0
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
